fad065f84aea0b649afe14454514f59270bf5784
Irina Efode <irinka.efode@gmail.com>
2017-10-25 01:50:13 +0300

    Add all components. FIX checkline()

--- polygon.py
+++ polygon.py
@@ -42,21 +42,31 @@ def addPointInRolygon():
 
 # generate random polygon
 def generatePolygon():
-    numberPoint = rnd.randint(3, 10)
+    numberPoint = rnd.randint(3, 6)
+    print numberPoint
     # print numberPoint
     while (points.__len__() != numberPoint):
         addPointInRolygon()
+    print points
 
 # generate random line
 def generateLinePoints():
     while(line.__len__() != 2):
-        points.append([rnd.randint(-10, 10),
+        line.append([rnd.randint(-10, 10),
                        rnd.randint(-10, 10)])
+    print line
 
 def checkLine():
     for currentPoint in points:
-        nextPoint = points.pop((points.index(currentPoint) + 1)) % points.__len__()
-        print nextPoint
+        nextPoint = points[(points.index(currentPoint) + 1) % points.__len__()]
+        if (currentPoint in line or
+            nextPoint in line):
+            return True
+        # print nextPoint
+        k1 = None
+        k2 = None
+        b1 = None
+        b2 = None
         if ((currentPoint[0] - nextPoint[0]) != 0):
             k1 = (nextPoint[1] - currentPoint[1])/(nextPoint[0] - currentPoint[0])
             b1 = k1*currentPoint[0] - currentPoint[1]
@@ -64,26 +74,37 @@ def checkLine():
             k2 = (line[1][1] - line[0][1]) / (line[1][0] - line[0][0])
             b2 = k2*line[0][0] - line[0][1]
 
+        maxLineY = max(line[0][1], line[1][1])
+        minLineY = min(line[0][1], line[1][1])
+        maxPointsY = max(currentPoint[1], nextPoint[1])
+        minPointsY = min(currentPoint[1], nextPoint[1])
+
         if (k1 == k2) and (b1 == b2):
             if (k1 == None and currentPoint[0] != line[0][0]):
                 continue
-            maxLineY = max(line[0][1], line[1][1])
-            minLineY = min(line[0][1], line[1][1])
-            maxPointsY = max(currentPoint[1], nextPoint[1])
-            minPointsY = min(currentPoint[1], nextPoint[1])
             if ((minLineY > maxPointsY) or
                 (maxLineY < minPointsY)):
                 continue
             else:
                 return True
-        else:
-            continue
+        elif (k2 == None):
+            y = k1 * line[1][0] + b1 - line[1][1]
+            if (minPointsY <= y <= maxPointsY):
+                return True
+            else:
+                continue
+        elif (k1 == None):
+            y = k2 * currentPoint[0] + b2 - currentPoint[1]
+            if (minLineY <= y <= maxLineY):
+                return True
+            else:
+                continue
 
         # test this
         y3 = k1 * line[0][0] + b1 - line[0][1]
         y4 = k2 * line[1][0] + b2 - line[1][1]
-        print y3
-        print y4
+        # print y3
+        # print y4
 
         if (y3 * y4 <= 0):
             return True
@@ -91,5 +112,55 @@ def checkLine():
             continue
     return False
 
-parseInputFile()
+def drawingPolygon(n):
+    # # drawing line for connection of first and last points
+    # draw_polygon = plt.Line2D((points[n - 1][0], points[0][0]),
+    #                           (points[n - 1][1], points[0][1]),
+    #                           lw = 2.5)
+    # plt.gca().add_line(draw_polygon)
+    # drawing polygon
+    i = 0
+    for i in range(n):
+        j = (i+1) % n
+        draw_polygon = plt.Line2D((points[i][0], points[j][0]),
+                                  (points[i][1], points[j][1]),
+                                  lw=2.5)
+        plt.gca().add_line(draw_polygon)
+
+def drawingLine():
+    #drawing line
+    draw_line = plt.Line2D((line[1][0], line[0][0]),
+                             (line[1][1], line[0][1]),
+                             lw=2.5,
+                             ls='-.',
+                             marker='.',
+                             markersize=50,
+                             markerfacecolor='r',
+                             markeredgecolor='r',
+                             alpha=0.5)
+    plt.gca().add_line(draw_line)
+
+def printInfo():
+    outputFile.write("Number of point: " + points.__len__().__str__())
+    outputFile.write("\nCoordinates of points of poligon:\n" + points.__str__())
+    outputFile.write("\nCoordinates of points of line:\n" + line.__str__())
+    # outputFile.write("\nLine and polygon: " + checkLine().__str__())
+    # print "\nLine and polygon: " + checkLine().__str__()
+
+# parseInputFile()
 generatePolygon()
+generateLinePoints()
+
+# printInfo()
+
+#drawing of objects
+drawingLine()
+# print points.__len__()
+# print points
+
+drawingPolygon(points.__len__())
+print checkLine()
+
+plt.axes()
+plt.axis('scaled')
+plt.show()
