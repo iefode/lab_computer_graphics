3bfb6c127abab2ff0a7d4f8d755c22023c92db21
Irina Efode <irinka.efode@gmail.com>
2017-11-13 23:24:42 +0300

    Fixed all for axis x

--- circle.py
+++ circle.py
@@ -23,7 +23,7 @@ def parseInputFile():
            circlesRadius.__len__() != 2):
         circlesCentres.append(parseStringToPoint())
         circlesRadius.append(int(inputFile.readline()))
-    circlesRadius.append(int(inputFile.readline()))
+    circlesRadius.append(float(inputFile.readline()))
 
 #FIXME r for arg is not exist
 def generateCircles():
@@ -33,6 +33,7 @@ def generateCircles():
         r = rnd.randint(0, 10)
         circlesCentres.append(point)
         circlesRadius.append(r)
+    #radius for conjugation
     circlesRadius.append(rnd.randint(0, 10))
 
 def checkRadius():
@@ -41,51 +42,72 @@ def checkRadius():
     d = (dx**2 + dy**2) ** 0.5
     d1 = circlesRadius[0] + circlesRadius[2]
     d2 = circlesRadius[1] + circlesRadius[2]
+    if(d1 + d2 == d):
+        x = (circlesCentres[0][0] * d1 + circlesCentres[1][0] * d2) / d
+        y = (circlesCentres[0][1] * d1 + circlesCentres[1][1] * d2) / d
+        circlesCentres.append([x, y])
+        return True
+    if (circlesRadius[0] + circlesRadius[1] == d):
+        if (circlesCentres[0][0] < circlesCentres[1][0]):
+            startPoint = circlesCentres[0]
+            r = circlesRadius[0]
+        else:
+            startPoint = circlesCentres[1]
+            r = circlesRadius[1]
+        x = startPoint[0] + d - r
+        y = 0
+        circlesCentres.append([x,y])
+
+        return True
     if (abs(d1 - d2) < d < d1 + d2):
         startPoint = circlesCentres[0]
         a = circlesRadius[2] - circlesRadius[1]
         b = circlesRadius[2] - circlesRadius[0]
         if (a + b <= d or
-            a + d <= b or
-            b + d <= a):
+                        a + d <= b or
+                        b + d <= a):
             return False
         if (circlesCentres[0][0] > circlesCentres[1][0]):
             startPoint = circlesCentres[1]
             tmp = a
             a = b
             b = tmp
-
+        #cos of angle in triangle
         angle = (b**2 + d**2 - a**2)/(2*d*b)
+        #projection of side triangle to d
         x = b * angle
         angle = (1 - angle**2)**0.5
+        #high in triangle
         y = b * angle
-
+        #converting relative coordinates (d) to absolute1
         if (circlesCentres[1][0] - circlesCentres[0][0]) != 0:
-            angle = (circlesCentres[1][1] - circlesCentres[0][1])/(circlesCentres[1][0] - circlesCentres[0][0])
+            k = float(circlesCentres[1][1] - circlesCentres[0][1])/(circlesCentres[1][0] - circlesCentres[0][0])
+            b = startPoint[1] - angle * startPoint[0]
+            angle = k
             angle = math.atan(angle)
             x = x*math.cos(angle) + startPoint[0]
         else:
+            k = None
+            b = None
             x += startPoint[0]
         if (circlesCentres[1][1] - circlesCentres[0][1]) != 0:
-            y1 = y*math.sin(angle) + startPoint[1]
-            y2 = -y*math.sin(angle) + startPoint[1]
+            tmp = k*x + b
+            y1 = y*math.sin(angle) + tmp
+            y2 = -y*math.sin(angle) + tmp
         else:
-            y1 = startPoint[1] + y
-            y2 = startPoint[1] - y
+            tmp = startPoint[1]
+            y1 = tmp + y
+            y2 = tmp - y
+        print tmp
+        print x
         circlesCentres.append([x, y1])
         circlesCentres.append([x, y2])
-        # else:
-        #     x = x + startPoint[0]
-        #     y = y + startPoint[1]
-        #     circlesCentres.append([x, y])
         return True
-    elif(d1 + d2 == d):
-        x = (circlesCentres[0][0] * d1 + circlesCentres[1][0] * d2) / d
-        y = (circlesCentres[0][1] * d1 + circlesCentres[1][1] * d2) / d
-        circlesCentres.append([x, y])
-        return True
-    else: #(d > d1 + d2 or d <= abs(d1 - d2)):
+    else:
         return False
+
+def findPoints():
+    points = []
 
 def drawingCircles():
     print circlesCentres
@@ -107,12 +129,13 @@ def drawingCircles():
                              color='r',
                              fill=False))
     ax.add_patch(circle[2])
-    circle.append(plt.Circle((circlesCentres[3][0],
+    if (circlesCentres.__len__() == 4):
+         circle.append(plt.Circle((circlesCentres[3][0],
                               circlesCentres[3][1]),
                              circlesRadius[2],
                              color='g',
                              fill=False))
-    ax.add_patch(circle[3])
+         ax.add_patch(circle[3])
 
 def drawingArc():
     return
